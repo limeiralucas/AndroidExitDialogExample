6dc9cb23836d44d28528b2a1e434b344a5933350
Lucas Limeira <lucasalveslm@gmail.com>
2018-04-24 17:41:19 -0300

    first commit

---
+++ app/src/main/java/com/lucaslimeira/dialogcommunication/ExitDialog.java
@@ -0,0 +1,45 @@
+package com.lucaslimeira.dialogcommunication;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+public class ExitDialog extends DialogFragment implements DialogInterface.OnClickListener {
+
+    private ExitListener listener;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        if(!(context instanceof ExitListener)) {
+            throw new RuntimeException("Not a ExitListener");
+        }
+
+        listener = (ExitListener)context;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Deseja Sair?");
+        builder.setPositiveButton("Sim", this);
+        builder.setNegativeButton("Não", this);
+
+        return builder.create();
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        if(which == DialogInterface.BUTTON_POSITIVE && listener != null) {
+            listener.onExit();
+        }
+    }
+
+    public interface ExitListener {
+        public void onExit();
+    }
+}
